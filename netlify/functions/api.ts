9031a6783c6e61aa291562465017c2b2dfed4e7e
Mr-Gre2n <mr.gre2n@gmail.com>
2025-11-28 17:41:23 +0200

    Add Deno lock file and update Netlify API function to use main app

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,13 +1,7 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-import express, { Router } from "express";
+import app from "../../src/index";
 import serverless from "serverless-http";
 
-const api = express();
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export const handler = serverless(app);
